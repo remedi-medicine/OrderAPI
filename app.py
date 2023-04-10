8c99cb72c3d37eb15417c547cbf56327d6e3b9d8
Burhanuddin Murtuza Fatehi <74709595+burhanuddin13@users.noreply.github.com>
2023-04-10 17:29:15 +0530

    Back to Basics

--- app.py
+++ app.py
@@ -8,27 +8,23 @@ client = razorpay.Client(auth=("rzp_test_nFKekgDUCv55L4", "onfHa3ohp93LrIt01HHYY
 @app.route('/create_order', methods=['POST'])
 def create_order():
   req_data = request.args
-  return(req_data)
-  # amount = req_data['amount']
-  # currency = req_data['currency']
-  # receipt = req_data['receipt']
-  # notes = req_data['notes']
-  # print(amount, currency, receipt, notes)
+  amount = req_data['amount']
+  currency = req_data['currency']
+  receipt = req_data['receipt']
+  notes = req_data['notes']
 
-  # order_data = {
-  #   "amount": 100*987,
-  #   "currency": "INR",
-  #   "receipt": "recipt#01",
-  #   "notes": {
-  #     "name": "Burhanuddin Fatehi"
-  #   }
-  # }
+  order_data = {
+    "amount": amount,
+    "currency": currency,
+    "receipt": receipt,
+    "notes": notes
+  }
 
-  # try:
-  #   order = client.order.create(order_data)
-  #   return order, 200
-  # except Exception as e:
-  #   return jsonify({"error": str(e)}), 500
+  try:
+    order = client.order.create(order_data)
+    return order, 200
+  except Exception as e:
+    return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
   app.run(debug=True)
